fe3d14880358253261390e0acb2ae3139c814c21
Pavel Druzhinin <p.a.druzhinin@tinkoff.ru>
2024-01-26 14:55:40 +0300

    Add tcp server + rot13 encryption logic

--- main.go
+++ main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+)
 
+func main() {
+	li, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		panic(err)
+	}
+
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	s := bufio.NewScanner(conn)
+	for s.Scan() {
+		ln := strings.ToLower(s.Text())
+		bs := []byte(ln)
+		r := rot13(bs)
+
+		fmt.Fprintf(conn, "%s - %s\n", ln, r)
+	}
+}
+
+func rot13(bs []byte) []byte {
+	var r13 = make([]byte, len(bs))
+	for i, v := range bs {
+		if v <= 109 {
+			r13[i] = v + 13
+		} else {
+			r13[i] = v - 13
+		}
+	}
+	return r13
 }
